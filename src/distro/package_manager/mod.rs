681c5a352ee136006a2e2cf7ca2f347656e651f6
martintc <kj4ntv@gmail.com>
2022-06-23 19:10:40 -0700

    Initial file for package manager

---
+++ src/distro/package_manager/mod.rs
@@ -0,0 +1,32 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum PackageManager {
+    Aptitude,
+    Dnf,
+    Pacman,
+    Portage,
+    Unknown,
+    Yum,
+    Zypper,
+}
+
+impl Default for PackageManager {
+    fn default() -> Self {
+        PackageManager::Unknown
+    }
+}
+
+impl Display for PackageManager {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match *self {
+            PackageManager::Aptitude => write!(f, "aptitde"),
+            PackageManager::Dnf => write!(f, "dnf"),
+            PackageManager::Pacman => write!(f, "pacman"),
+            PackageManager::Portage => write!(f, "portage"),
+            PackageManager::Yum => write!(f, "yum"),
+            PackageManager::Zypper => write!(f, "zypper"),
+            _ => write!(f, "{:?}", self),
+        }
+    }
+}
