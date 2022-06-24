2dac2fdd6d5ebde60f163548eb5c6afa50d3c4b9
martintc <kj4ntv@gmail.com>
2022-06-23 22:07:07 -0700

    Implement copy trait on family

--- src/family.rs
+++ src/family.rs
@@ -1,7 +1,7 @@
 use std::fmt::{self, Display, Formatter};
 use os_info::Type;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Family {
     ArchBased,
     DebianBased,
