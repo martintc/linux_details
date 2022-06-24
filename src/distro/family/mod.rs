6edb35600336210b6c13fbc5eab8e9bae4b1da5b
martintc <kj4ntv@gmail.com>
2022-06-23 19:54:44 -0700

    Implement get family function

--- src/distro/family/mod.rs
+++ src/distro/family/mod.rs
@@ -5,8 +5,10 @@ pub enum Family {
     ArchBased,
     DebianBased,
     GentooBased,
+    Independent,
     RedhatBased,
     SlackwareBased,
+    SuseBased,
     UbuntuBased,
     Unknown,
 }
@@ -22,9 +24,11 @@ impl Display for Family {
         match *self {
             Family::ArchBased => write!(f, "arch-based"),
             Family::DebianBased => write!(f, "debian-based"),
-            Family::GentooBased => Write!(f, "gentoo-based"),
+            Family::GentooBased => write!(f, "gentoo-based"),
+            Family::Independent => write!(f, "independent"),
             Family::RedhatBased => write!(f, "redhat-based"),
             Family::SlackwareBased => write!(f, "slackware-based"),
+            Family::SuseBased => write!(f, "suse-based"),
             Family::UbunutBased => write!(f, "ubuntu-based"),
             _ => write!(f, "{:?}", self),
         }
@@ -32,8 +36,33 @@ impl Display for Family {
 }
 
 impl Family {
-    pub fn get_family() -> Family {
-        
+    pub fn get_family(os_type: os_info::Type) -> Family {
+        match os_type {
+            Type::Alpine => Family::Independent,
+            Type::Amazon => Family::RedhatBased,
+            Type::Android => Family::Independent,
+            Type::Arch => Family::ArchBased,
+            Type::CentOs => Family::RedhatBased,
+            Type::Debian => Family::Debian,
+            Type::EndeavourOS => Family::ArchBased,
+            Type::Fedora => Family::RedhatBased,
+            Type::Gentoo => Family::GentooBased,
+            Type::Linux => Family::Independent,
+            Type::Manjaro => Family::ArchBased,
+            Type::Mint => Family::UbuntuBased,
+            Type::NixOS => Family::Independent,
+            Type::openSUSE => Family::SuseBased,
+            Type::OracleLinux => Family::RedhatBased,
+            Type::Pop => Family::Ubuntu,
+            Type::Raspbian => Family::DebianBased,
+            Type::Redhat => Family::RedhatBased,
+            Type::RedHatEnterprise => Family::RedhatBased,
+            Type::Solus => Family::Independent,
+            Type::SUSE => Family::SuseBased,
+            Type::Ubuntu => Family::UbuntuBased,
+            Type::Garuda => Family::ArchBased,
+            _ => Family::Independent,
+        }
     }
 }
 
